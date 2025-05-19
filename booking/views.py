222af900d54aea23533d99fb516c5cf4ae1a62c8
HazelHawadi <hawadihazel@icloud.com>
2025-05-19 01:04:11 +0100

    create about and classes page

--- booking/views.py
+++ booking/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'booking/index.html')
 
-
 def home(request):
     return render(request, 'booking/index.html')
+
+def about(request):
+    return render(request, 'booking/about.html')
+
+def classes(request):
+    return render(request, 'booking/classes.html')
